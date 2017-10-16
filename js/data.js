53637c649c80dc5d1c885699e5510a4fa7f5e5c2
honzi <czechinthemail@gmail.com>
2017-10-15 17:53:54 -0700

    moved uncommon functions to data.js

---
+++ js/data.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function decode(){
+    try{
+        document.getElementById('binary-data').value =
+          atob(document.getElementById('characters').value);
+
+    }catch(error){
+        document.getElementById('binary-data').value = '';
+    }
+}
+
+function encode(){
+    document.getElementById('characters').value =
+      btoa(document.getElementById('binary-data').value);
+}
--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
 'use strict';
 
-function decode(){
-    try{
-        document.getElementById('binary-data').value =
-          atob(document.getElementById('characters').value);
-
-    }catch(error){
-        document.getElementById('binary-data').value = '';
-    }
-}
-
-function encode(){
-    document.getElementById('characters').value =
-      btoa(document.getElementById('binary-data').value);
-}
-
 function repo_init(){
     core_repo_init({
       'title': 'BinaryDataEndecoder.htm',
