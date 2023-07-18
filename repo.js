fa408a9876830d5568320ecf79bad828a06c4eb6
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 12:43:17 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -12,3 +12,17 @@ function decode(){
 function encode(){
     document.getElementById('characters').value = btoa(document.getElementById('binary-data').value);
 }
+
+function repo_init(){
+    core_repo_init({
+      'events': {
+        'binary-data': {
+          'oninput': encode,
+        },
+        'type': {
+          'onchange': encode,
+        },
+      },
+      'title': 'BinaryDataEndecoder.htm',
+    });
+}
